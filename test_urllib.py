e69234ee8f75d90cc003fe1e960adeb7ba01f343
Pauline <38728498+PaulineGHG@users.noreply.github.com>
2019-05-07 16:51:13 +0200

    Update test_urllib.py

--- test_urllib.py
+++ test_urllib.py
@@ -3,10 +3,16 @@ import urllib.request
 url="https://graphhopper.com/api/1/route?point=51.131,12.414&point=48.224,3.867&vehicle=car&locale=de&calc_points=false&key=1620b7ee-90b2-4daa-9ef5-4aba2d279978"
 
 
-fp=urllib.request.urlopen(url)
-contenu=fp.read().decode("utf-8")
+try:
+    fp=urllib.request.urlopen(url)
+    contenu=fp.read().decode("utf-8")
 
-print(contenu)
+    print(contenu)
+except HTTPError as e:
+    print(e)
+    print("-", e.code)
+    print("--", e.reason)
+    print("---", e.headers)
 
 
 
