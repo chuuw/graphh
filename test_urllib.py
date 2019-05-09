641dc345c07924ae8255d45683e5b5cb3d4fd6c5
caillardc <caillardclement@outlook.fr>
2019-05-09 15:29:11 +0200

    Update test_urllib.py

--- test_urllib.py
+++ test_urllib.py
@@ -1,6 +1,7 @@
 import urllib.request
+from urllib.error import HTTPError
 
-url="https://graphhopper.com/api/1/route?point=51.131,12.414&point=48.224,3.867&vehicle=car&locale=de&calc_points=false&key=1620b7ee-90b2-4daa-9ef5-4aba2d279978"
+url="https://graphhopper.com/api/1/route?point=51.131,12.414&point=48.224,3.867&vehicle=ezfke&locale=de&calc_points=false&key=1620b7ee-90b2-4daa-9ef5-4aba2d279978"
 
 
 try:
