f1ff6a55e79b8983e9642c27f6ca21dec29fc51c
Pauline <38728498+HoshigakiSame@users.noreply.github.com>
2019-05-09 15:17:18 +0200

    Update test_urllib.py

--- test_urllib.py
+++ test_urllib.py
@@ -1,6 +1,6 @@
 import urllib.request
 
-url="https://graphhopper.com/api/1/route?point=51.131,12.414&point=48.224,3.867&vehicle=caru&locale=de&calc_points=false&key=1620b7ee-90b2-4daa-9ef5-4aba2d279978"
+url="https://graphhopper.com/api/1/route?point=51.131,12.414&point=48.224,3.867&vehicle=car&locale=de&calc_points=false&key=1620b7ee-90b2-4daa-9ef5-4aba2d279978"
 
 
 try:
