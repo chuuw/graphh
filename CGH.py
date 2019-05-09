ddfdf95f326fc051b200b1406c3e2f9130eeb1d7
Pauline <38728498+HoshigakiSame@users.noreply.github.com>
2019-05-09 15:54:19 +0200

    Update CGH.py

--- CGH.py
+++ CGH.py
@@ -1,10 +1,16 @@
 import urllib.request
 import json
 
+# url complet :
+# https://graphhopper.com/api/1/route?point=51.131,12.414&point=48.224,3.867&
+# vehicle=ezfke&locale=de&calc_points=false&key=1620b7ee-90b2-4daa-9ef5-4aba2d279978
 
 class GraphHopper(object):
+    url = "https://graphhopper.com/api/1/"
 
     def __init__(self, Ak):
         self.APIkey = Ak
     #initialisation de la classe
+
+
 
