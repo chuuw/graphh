10b73284ce7b1bd4f5340b076fc63e3b6634e688
chuuw <50207670+chuuw@users.noreply.github.com>
2019-05-14 21:58:27 +0200

    test utilisation fonction url

--- graphh/test/test_CGHfonctionurl_maëlle.py
+++ graphh/test/test_CGHfonctionurl_maëlle.py
@@ -25,17 +25,30 @@ class GraphHopper(object):
         fp = urllib.request.urlopen(complete_url)
         return json.load(fp)
 
+    def reverse_geocode(self, latlong):
+        # prend en entrée un tuple (la lattitude et la longitude)
+        # retourne un dictionnaire
+        l_param = []
+        l_param.append("point={},{}".format(latlong[0],latlong[1]))
+        l_param.append("key={}".format(self.APIkey))
+        # et ainsi de suite pour chaque paramètre renseigné
+        l_param.append("reverse=true")
+        return self.url_handle("geocode", l_param)
+
 fp1 = open("credentials.json", "r" , encoding = "utf-8")
 dossiercle = json.load(fp1)
 key_access = dossiercle["graphhopper"]
 G1 = GraphHopper(key_access)
 
 #test api route
-a="route"
-l=["point=51.131,12.414","point=48.224,3.867","key=1620b7ee-90b2-4daa-9ef5-4aba2d279978"]
-print(G1.url_handle(a, l))
+#a="route"
+#l=["point=51.131,12.414","point=48.224,3.867","key=1620b7ee-90b2-4daa-9ef5-4aba2d279978"]
+#print(G1.url_handle(a, l))
 
 #test api geocode
-a2="geocode"
-l2=["point=48.1572091,-1.6853144","key=1620b7ee-90b2-4daa-9ef5-4aba2d279978","reverse=true"]
-print(G1.url_handle(a2, l2))
+#a2="geocode"
+#l2=["point=48.1572091,-1.6853144","key=1620b7ee-90b2-4daa-9ef5-4aba2d279978","reverse=true"]
+#print(G1.url_handle(a2, l2))
+
+#test reverse_geocode
+print(G1.reverse_geocode((48.1572091,-1.6853144)))
