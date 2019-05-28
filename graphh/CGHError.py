aeafca8dd6aa30f789f3db83296c1347144f3628
caillardc <caillardclement@outlook.fr>
2019-05-28 15:49:18 +0200

    j'ai fait nimp je répare

--- graphh/CGHError.py
+++ graphh/CGHError.py
@@ -4,65 +4,77 @@ import sys
 import json
 
 
-def valid_point(point):  # point = (lat, long)
-  try:
-   for coordinate in point:
-     float(coordinate)
-  except ValueError:
-    print("Error: coordinates are not valid")
-    sys.exit()
-    return False
-  else:
-    if not len(point)==2:
-      print("Error: point need to be lat and long")
-      sys.exit()
-      return False
-    elif not (-90 <= point[0] <= 90 and -180 <= point[1] <= 180) :
-      print("Error: latitude or longitude value is not valid")
-      sys.exit()
-      return False
-  return True
+def check_point(point):
+    try:
+        for coordinate in point:
+            float(coordinate)
+    except ValueError as e:
+        e = ValueError("Coordinates are not valid")
+        raise e
+    else:
+        if not len(point) == 2:
+            e = ValueError("Point needs to be lat and long")
+            raise e
+        elif not (-90 <= point[0] <= 90 and -180 <= point[1] <= 180):
+            e = ValueError("Latitude or longitude value is not valid")
+            raise e
 
-def valid_vehicle(vehicle):
-  l_vehicle = [
-      "car", "small_truck", "truck",
-      "scooter", "foot", "hike",
-      "bike", "mtb", "racingbike"
-      ]
-  if vehicle in l_vehicle:
+
+def check_vehicle(vehicle):
+    l_vehicle = [
+        "car", "small_truck", "truck",
+        "scooter", "foot", "hike",
+        "bike", "mtb", "racingbike"
+    ]
+    if not vehicle in l_vehicle:
+        e = ValueError("{} is not a valid vehicle".format(vehicle))
+        raise e
+
+
+def check_locale(locale):
+    l_locale = ["en", "fr", "de", "it"]
+    if not locale in l_locale:
+        e = ValueError("{} is not a valid language".format(locale))
+        raise e
+
+
+def valid_unittime(unit):
+  l_unit = ["ms", "s", "min", "h"]
+  if unit in l_unit :
     return True
   else:
-    print("Error: wrong vehicle")
+    print("Error : wrong time unit")
     sys.exit()
 
-def valid_locale(locale):
-  l_locale = ["en", "fr", "de", "it"]
-  if locale in l_locale:
+
+def valid_unitdistance(unit):
+  l_unit = ["m", "km"]
+  if unit in l_unit :
     return True
   else:
-    print("Error: wrong language")
+    print("Error : wrong distance unit")
     sys.exit()
 
-def CGHError(url):
-  try:
-    fp=urllib.request.urlopen(url)
-    contenu=json.load(fp)
-  except HTTPError as e:
+
+def CGHError(e):
     if e.code == 400:
-      print("Error: argument not correct")
+      e.msg= "argument not correct"
+      raise HTTPError
       print(APIKeyRemaining(e), "remaining credits")
     elif e.code == 401:
-      print("Error: key error")
+      e.msg= "key error"
+      raise HTTPError
     elif e.code == 429:
-      print("Error: API limit reached")
+      e.msg= "API limit reached"
+      raise HTTPError
     elif e.code == 500:
-      print("Error: Internal server error")
+      e.msg= "Internal server error"
+      raise HTTPError
       print(APIKeyRemaining(e), "remaining credits")
     elif e.code == 501:
-      print("Error: Vehicle error")
+      e.msg = "Vehicle error"
+      raise HTTPError
       print(APIKeyRemaining(e), "remaining credits")
-    sys.exit()
-  return True
 
 
 def APIKeyRemaining(error):
