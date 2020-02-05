8723a892a6aa86acaf6ee45356d640537760e59b
Yann <yann.causeur@yahoo.fr>
2020-02-05 11:21:18 +0100

    Update CGH.py
    print test

--- graphh/CGH.py
+++ graphh/CGH.py
@@ -5,6 +5,7 @@ import unicodedata
 from graphh import CGHError
 from urllib.request import urlopen
 from urllib.error import HTTPError
+print test
 
 class GraphHopper(object):
     """GraphHopper API class.
