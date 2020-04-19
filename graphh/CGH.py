b0f6a0c05070576e072a7673741486390657a07f
Florian-collab <57749324+Florian-collab@users.noreply.github.com>
2020-04-19 12:55:07 +0200

    Update CGH.py
    Just edit the docstring errors

--- graphh/CGH.py
+++ graphh/CGH.py
@@ -9,40 +9,39 @@ from graphh import CGHError
 
 
 class GraphHopper:
-    """GraphHopper API class.
+   """GraphHopper API class.
 
-        Parameters
-        ----------
-         api_key: str
-            API key to be used for queries
-         premium: bool
-            Whether the account corresponding to this key is a premium account
-            or not
-    """
-    """"
+    Parameters
+    ----------
+     api_key: str
+        API key to be used for queries
+     premium: bool
+        Whether the account corresponding to this key is a premium account
+        or not
+
     Examples
-        --------
-        >>> from graphh import GraphHopper
-        >>> gh_client = GraphHopper(api_key=YOUR_API_KEY)
-        >>> gh_client.address_to_latlong("Rennes, République")
-                (48.1098593, -1.6787526)
-        >>> latlong_Paris = gh_client.address_to_latlong("Paris")
-        >>> latlong_Madrid = gh_client.address_to_latlong("Madrid")
-        >>> gh_client.distance([latlong_Paris, latlong_Madrid], unit="km")
-                1269.9657
-        >>> gh_client.duration([latlong_Paris, latlong_Madrid], unit="h")
-                11.641364444444443
-        >>> import pprint
-        >>> pprint.pprint(gh_client.route([latlong_Paris, latlong_Madrid]))
-                {'hints': {'visited_nodes.average': '947.0', 'visited_nodes.sum': '947'},
-                        'info': {'copyrights': ['GraphHopper', 'OpenStreetMap contributors'],
-                            'took': 43},
-                        'paths': [{'ascend': 11624.469142794609,
-                        'bbox': [-3.778313, 40.412748, 2.346683, 48.878851],
-                        'descend': 11026.474138140678,
-                        'details': {},
-                        'distance': 1269965.7,
-                        'instructions': [{'distance': 246.715,
+    --------
+    >>> from graphh import GraphHopper
+    >>> gh_client = GraphHopper(api_key=YOUR_API_KEY)
+    >>> gh_client.address_to_latlong("Rennes, République")
+    (48.1098593, -1.6787526)
+    >>> latlong_Paris = gh_client.address_to_latlong("Paris")
+    >>> latlong_Madrid = gh_client.address_to_latlong("Madrid")
+    >>> gh_client.distance([latlong_Paris, latlong_Madrid], unit="km")
+    1269.9657
+    >>> gh_client.duration([latlong_Paris, latlong_Madrid], unit="h")
+    11.641364444444443
+    >>> import pprint
+    >>> pprint.pprint(gh_client.route([latlong_Paris, latlong_Madrid]))
+    {'hints': {'visited_nodes.average': '947.0', 'visited_nodes.sum': '947'},
+     'info': {'copyrights': ['GraphHopper', 'OpenStreetMap contributors'],
+              'took': 43},
+     'paths': [{'ascend': 11624.469142794609,
+                'bbox': [-3.778313, 40.412748, 2.346683, 48.878851],
+                'descend': 11026.474138140678,
+                'details': {},
+                'distance': 1269965.7,
+                'instructions': [{'distance': 246.715,
                                   'heading': 165.02,
                                   'interval': [0, 2],
                                   'sign': 0,
@@ -51,19 +50,13 @@ class GraphHopper:
                                   'time': 67674},
     ...
     """
-
-
     url = "https://graphhopper.com/api/1/"
 
     def __init__(self, api_key, premium=False):
-
         self.api_key = api_key
         self.premium = premium
 
-
-
     def _url_requests(self, api, data, request="get"):
-
         """ This function does an url request ( GET or POST) with given parameters
 
         Parameters
@@ -72,13 +65,16 @@ class GraphHopper:
             name of the api used
         data: dict
             dict of parameters to insert in the url
+        requests: str, optional
+            The type of request : GET or POST
+            By default, the request will be in GET
 
         Returns
         -------
         dict
             The dictionary return by url request (GET or POST)
+            
         """
-
         complete_url = GraphHopper.url + api + "?"
 
         if request.upper() == "POST":
@@ -99,22 +95,21 @@ class GraphHopper:
 
 
     def _latlong_handle_request(self,l_latlong,request="get"):
-
         """" This function changes the format of the coordinates according to the request
 
         Parameters
         ----------
         l_latlong: list
             The list of list (latitude, longitude) of the considerated points
-        request: str, optional
-            motion of request
+        requests: str, optional
+            The type of request : GET or POST
+            By default, the request will be in GET
 
         Returns
         -------
         list
             list of the coordinates according to the request
         """
-
         l_latlong_handle = []
         if request.upper() == "POST":
             for latlong in l_latlong:
@@ -128,7 +123,6 @@ class GraphHopper:
 
 
     def geocode(self, address, limit=1, locale="en"):
-
         """This function does geocoding.
         It transforms a given address into matching geographic coordinates.
 
@@ -150,7 +144,6 @@ class GraphHopper:
             including their geographic coordinates, and the number of ms it
             took.
         """
-
         a = str(unicodedata.normalize('NFKD',str(address)).encode('ascii', 'ignore'))
         data = dict()
         data["q"] = "{}".format(a.replace(" ", "+"))
@@ -161,7 +154,6 @@ class GraphHopper:
 
 
     def reverse_geocode(self, latlong, locale="en"):
-
         """This function does reverse geocoding.
                 It transforms given geographic coordinates into matching addresses.
 
@@ -181,7 +173,6 @@ class GraphHopper:
             A dictionary containing the matching locations' information,
             including their addresses, and the number of ms it took.
         """
-
         data = dict()
         data["reverse"] = "true"
         myCGHError.check_point([latlong], "geocode")
@@ -190,19 +181,18 @@ class GraphHopper:
         return self._url_requests("geocode", data)
 
 
-
     def route(self,  l_latlong, request="get", vehicle="car",
                     locale="en", calc_points="true", instructions="true",
                     points_encoded="true", elevation="false"):
-
         """This function give an itinerary between given points
 
         Parameters
         ----------
         l_latlong : list
             The list of list (latitude, longitude) of the considerated points
-        request: str
-            motion of request
+        requests: str, optional
+            The type of request : GET or POST
+            By default, the request will be in GET
         vehicle : str, optional
             The type of vehicle chosen in the list : ["car", "foot", "bike"]
             if the acount is not premium
@@ -235,7 +225,6 @@ class GraphHopper:
             instructions with street name and description what the user has to
             do in order to follow the route.
         """
-
         data = dict()
 
         myCGHError.check_point(l_latlong, "route")
@@ -265,7 +254,6 @@ class GraphHopper:
 
     def matrix_request(self, l_from_points, l_to_points,request="get",
                     vehicle="car"):
-
         """This function gives the different possible matrix
             between the points: distance, temp, weight
 
@@ -275,8 +263,9 @@ class GraphHopper:
         The list of list (latitude, longitude) of the starting points for the routes
         l_to_points : list
             The tuple list (latitude, longitude) of the destination points for the routes
-        request: str
-            motion of request
+        requests: str, optional
+            The type of request : GET or POST
+            By default, the request will be in GET
         vehicle : str, optional
             The type of vehicle chosen in the list : ["car", "foot", "bike"]
             if the acount is not premium
@@ -287,7 +276,6 @@ class GraphHopper:
         dict
             A dictionary containing distances, times and weights matrices
         """
-
         data = dict()
 
         myCGHError.check_point(l_from_points, "matrix")
@@ -312,7 +300,6 @@ class GraphHopper:
 
 
     def maxtrix(self,l_from_address, l_to_address,out_array, format="default", vehicle="car", request="get"):
-
         """This function gives one matrix between the points: distances, times or weights
 
         Parameters
@@ -321,13 +308,22 @@ class GraphHopper:
             The list containing the cities, address of the points
         l_to_address : list
             The list containing the cities, address of the points
-        request: str
-            motion of request
+        out_array: str
+            Specifies which array should be included in the response from the list ["distances", "times", "weights"]
+            The units of the entries of distances are meters, 
+            of times are seconds and of weights is arbitrary and
+            it can differ for different vehicles or versions of this API
+        format: str, optional
+            Specifies how the array should be drawn from the list ["pandas","numpy","default"]
+            By default, the array is default        
         vehicle : str, optional
             The type of vehicle chosen in the list : ["car", "foot", "bike"]
             if the acount is not premium
             And can be chosen in the list : ["small_truck", "truck", "scooter",
             "hike", "mtb", "racingbike"] if it is
+        requests: str, optional
+            The type of request : GET or POST
+            By default, the request will be in GET
 
         Returns
         -------
@@ -340,7 +336,6 @@ class GraphHopper:
             liste : list
                 A list of list data of the function matrix
         """
-
         myCGHError.check_out_array(out_array)
         myCGHError.check_format_matrix(format)
         l_from_points = list()
@@ -364,9 +359,7 @@ class GraphHopper:
             return liste
 
 
-
     def address_to_latlong(self, address):
-
         """This function is a simplified version of the geocoding function.
 
         Parameters
@@ -382,7 +375,6 @@ class GraphHopper:
             The first element is the latitude and the second one is the
             longitude.
         """
-
         d = self.geocode(address, limit=1)
         lat = d["hits"][0]["point"]["lat"]
         lng = d["hits"][0]["point"]["lng"]
@@ -392,7 +384,6 @@ class GraphHopper:
 
 
     def latlong_to_address(self, latlong):
-
         """This function is a simplified version of the reverse geocoding
             function.
 
@@ -408,7 +399,6 @@ class GraphHopper:
         str
             The address of the location.
         """
-
         d = self.reverse_geocode(latlong)
         l_elem = []
         if "housenumber" in d["hits"][0].keys():
@@ -441,7 +431,6 @@ class GraphHopper:
 
 
     def distance(self, l_latlong, unit="m"):
-
         """This function give the distance between precised points for a given
                 itinerary
 
@@ -459,7 +448,6 @@ class GraphHopper:
         float
             The number of the distance for the itinerary for the unit chosen
         """
-
         dic = self.route(l_latlong, points_encoded="flase")
         myCGHError.check_unitdistance(unit)
         if unit == "m":
@@ -470,7 +458,6 @@ class GraphHopper:
 
 
     def duration(self, l_latlong, vehicle="car", unit="ms"):
-
         """This function give the time between precised points for a given
                 itinerary
 
@@ -496,7 +483,6 @@ class GraphHopper:
             The number of the time for the itinerary for the unit and vehicle
             chosen
         """
-
         dic = self.route(l_latlong, vehicle, points_encoded="false")
         myCGHError.check_unittime(unit)
         if unit == "ms":
@@ -508,8 +494,8 @@ class GraphHopper:
         elif unit == "h":
             return (((dic["paths"][0]["time"]) / 1000) / 60) / 60
 
+        
     def elevation_point(self, latlong):
-
         """This function give an elevation for a given geographic coordinates
 
         Parameters
@@ -524,7 +510,6 @@ class GraphHopper:
         float
             Elevation of one geographic coordinate couple
         """
-
         dict = self.route([latlong, latlong], instructions="false",
                           elevation="true", points_encoded="false")
         return dict["paths"][0]["points"]["coordinates"][0][2]
